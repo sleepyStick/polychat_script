04d74966517a072557b303718ddd1312a6eca447
Henry Evans <hevans03@calpoly.edu>
2022-04-13 22:53:27 -0700

    get polychats based on securityID of RA

--- make_request.py
+++ make_request.py
@@ -21,10 +21,15 @@ def main():
     #send the actual request
     API_auth = requests.auth.HTTPBasicAuth(email, API_key)
     headers = {'Accept': 'application/json'}
-    query = f'SELECT NameLast, NameFirst FROM Booking JOIN Entry WHERE RoomSpace like "172D-2%" And EntryStatusEnum = "5" ORDER BY Entry.NameLast'
+    #for getting all residents on a given floor
+    #query = f'SELECT NameLast, NameFirst FROM Booking JOIN Entry WHERE RoomSpace like "172D-2%" And EntryStatusEnum = "5" ORDER BY Entry.NameLast'
+    secID = 898
+    query = f'SELECT Title, EntryID, CreatedBy_SecurityUserID FROM Program JOIN ProgramEntry WHERE Community = "yakʔitʸutʸu 2" And DateCreated > "2022-03-01T00:00:00.001-08:00" And CreatedBy_SecurityUserID = "{secID}" ORDER BY Program.DateCreated'
     response = requests.get(f'https://calpoly.starrezhousing.com/StarRezREST/services/query?q={quote(query)} / GET', 
     headers=headers,
     auth=API_auth).json()
+    with open('outfile.json', 'w') as outfile:
+        json.dump(response, outfile)
 
     print(response)
 
